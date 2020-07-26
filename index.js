a412bef99fa73a0ee9ec00c25dd767ccc5ac54b6
Carolyn Stransky <carolynstransky@gmail.com>
2020-07-26 23:00:20 +0200

    :revolving_hearts: Write initial queries and resolvers

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log("In the name of the moon, I'll punish you 🌙");
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+let scouts = [
+  {
+    id: "1",
+    scoutName: "Sailor Moon",
+    japaneseName: "Usagi Tsukino (月野 うさぎ)",
+    englishName: "Serena Tsukino",
+    affiliation: "Inner Sailor Scouts",
+    guardianCat: true
+  },
+  {
+    id: "2",
+    scoutName: "Sailor Mercury",
+    japaneseName: "Ami Mizuno (水野 亜美)",
+    englishName: "Amy Anderson",
+    affiliation: "Inner Sailor Scouts",
+    guardianCat: false
+  },
+  {
+    id: "3",
+    scoutName: "Sailor Mars",
+    japaneseName: "Rei Hino (火野 レイ)",
+    englishName: "Raye Hino",
+    affiliation: "Inner Sailor Scouts",
+    guardianCat: false
+  },
+  {
+    id: "4",
+    scoutName: "Sailor Jupiter",
+    japaneseName: "Makoto Kino (木野 まこと)",
+    englishName: "Lita Kino",
+    affiliation: "Inner Sailor Scouts",
+    guardianCat: false
+  },
+  {
+    id: "5",
+    scoutName: "Sailor Venus",
+    japaneseName: "Minako Aino (愛野 美奈子)",
+    englishName: "Mina Aino",
+    affiliation: "Inner Sailor Scouts",
+    guardianCat: true
+  }
+];
+
+const resolvers = {
+  Query: {
+    scouts: () => scouts,
+    scout: (parent, args) => {
+      return scouts.find(scout => scout.id === args.id);
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🌙 Sailor scout server ready at ${url}`);
+});
--- schema.js
+++ schema.js
@@ -1,7 +1,19 @@
 const { gql } = require("apollo-server");
 
+// Apollo Server requires either an existing schema, modules or typeDefs
 const typeDefs = gql`
-  # Schema goes here!
+  type Scout {
+    id: ID!
+    scoutName: String!
+    japaneseName: String!
+    englishName: String
+    affiliation: String
+    guardianCat: Boolean
+  }
+  type Query {
+    scouts: [Scout]!
+    scout(id: ID!): Scout
+  }
 `;
 
 module.exports = typeDefs;
